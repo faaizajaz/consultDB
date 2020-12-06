decc1ef7d3d31b5abb98337ad19fa2341cf38be5
faaiz <faaizajaz@gmail.com>
2020-12-06 18:45:43 +0500

    Added project code, star rating, comment fields, and __str__ method for ratings

--- rating/models.py
+++ rating/models.py
@@ -10,3 +10,9 @@ class Rating(models.Model):
 
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     consultant = models.ForeignKey(Consultant, on_delete=models.CASCADE)
+    project_code = models.CharField(verbose_name="OPM project code", max_length=10)
+    star_rating = models.IntegerField(verbose_name="Rating from 1-5", default=0)
+    comment = models.TextField(verbose_name="Additional comments")
+
+    def __str__(self):
+        return f'Rating for {self.consultant} by {self.creator}'
