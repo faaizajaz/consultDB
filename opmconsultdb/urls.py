2bd5bf06d32e82d69ebaecafa9a7b2984282adf8
faaiz <faaizajaz@gmail.com>
2020-12-06 18:46:18 +0500

    Added route for consultant query

--- opmconsultdb/urls.py
+++ opmconsultdb/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import query.views as QueryViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('search/', QueryViews.ConsultantQueryView, name='consultant-query')
 ]
