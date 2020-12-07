dcb5a67a24b31f7c62e64920e41ac33d4d59a4e3
faaiz <faaizajaz@gmail.com>
2020-12-07 18:48:45 +0500

    Added natural keys to expertise models

--- expertise/models.py
+++ expertise/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 
+class PracticeAreaManager(models.Manager):
+
+    def get_by_natural_key(self, name):
+        return self.get(name=name)
+
 class PracticeArea(models.Model):
     """
     Description: The broad practice area/s of expertise. Align with portfolios.
@@ -8,9 +13,17 @@ class PracticeArea(models.Model):
 
     name = models.CharField(verbose_name="Practice area name", max_length=500)
 
+    objects = PracticeAreaManager()
+
     def __str__(self):
         return f'{self.name}'
 
+    def natural_key(self):
+        return(self.name)
+
+class SpecializationManager(models.Manager):
+    def get_by_natural_key(self, name):
+        return self.get(name=name)
 
 class Specialization(models.Model):
     """
@@ -20,9 +33,18 @@ class Specialization(models.Model):
     name = models.CharField(verbose_name="Specialization name", max_length=500)
     practice_area = models.ForeignKey(PracticeArea, on_delete=models.CASCADE)
 
+    objects = SpecializationManager()
+
     def __str__(self):
         return f'{self.name} ({self.practice_area})'
 
+    def natural_key(self):
+        return(self.name)
+
+class SkillManager(models.Manager):
+
+    def get_by_natural_key(self, name):
+        return self.get(name=name)
 
 class Skill(models.Model):
     """
@@ -31,5 +53,10 @@ class Skill(models.Model):
 
     name = models.CharField(verbose_name="Skill name", max_length=500)
 
+    objects = SkillManager()
+
     def __str__(self):
         return f'{self.name}'
+
+    def natural_key(self):
+        return(self.name)
