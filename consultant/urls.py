40429ee83781fc13076532f4f65d356c79ec25cf
faaiz <faaizajaz@gmail.com>
2020-12-08 09:53:42 +0500

    Adding consultant registration views

--- consultant/urls.py
+++ consultant/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<consultant_id>', views.ConsultantView.as_view(), name='consultant-view')
+    path('add_consultant/', views.BioFormView, name='bioform'),
+    path('<consultant_id>/', views.ConsultantView.as_view(), name='consultant-view'),
+    path('<consultant_id>/pa/', views.PracticeAreaFormView, name='practice-area-view'),
+    path('<consultant_id>/sp/', views.SpecializationFormView, name='specializaion-view')  
 ]
--- consultant/views.py
+++ consultant/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 from .models import Consultant
 from django.shortcuts import get_object_or_404
+from .forms import BioForm, PracticeAreaForm
+from django import forms
 
 class ConsultantView(generic.DetailView):
     template_name = 'consultant/consultant-view.html'
@@ -9,3 +11,34 @@ class ConsultantView(generic.DetailView):
 
     def get_object(self):
         return get_object_or_404(Consultant, pk=self.kwargs['consultant_id'])
+
+def BioFormView(request, **kwargs):
+
+    if request.method =='POST':
+        form = BioForm(request.POST)
+        if form.is_valid():
+            new_consultant = form.save()
+            print(new_consultant.id)
+            return redirect('practice-area-view', consultant_id=new_consultant.id)
+    else:
+        form = BioForm()
+    return render(request, 'consultant/add-consultant.html', {'form': form})
+
+def PracticeAreaFormView(request, **kwargs):
+    consultant = Consultant.objects.get(id=kwargs['consultant_id'])
+    if request.method == 'POST':
+
+        form = PracticeAreaForm(request.POST, instance=consultant)
+        if form.is_valid():
+            form.save()
+            print(consultant.practicearea_set.all())
+            print(type(consultant.practice_areas))
+            return redirect('specialization-view', consultant_id=consultant.id)
+    else:
+        form = PracticeAreaForm(instance=consultant)
+    return render(request, 'consultant/add-consultant.html', {'form': form})
+
+def SpecializationFormView(request, **kwargs):
+    consultant = Consultant.objects.get(id=kwargs['consultant_id'])
+
+
