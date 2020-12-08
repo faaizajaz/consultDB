8af58cb1f394300fc1fe6f146da9c793624cf17f
faaiz <faaizajaz@gmail.com>
2020-12-08 09:52:33 +0500

    Added consultant registration forms

---
+++ consultant/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from .models import Consultant
+from expertise.models import PracticeArea, Specialization, Skill
+
+
+class BioForm(forms.ModelForm):
+    """
+    Description: The first of three consultant creation forms. This creates the
+                 consultant and populates the initial data.
+    """
+    first_name = forms.CharField(required=True, max_length=500)
+    last_name = forms.CharField(required=True, max_length=500)
+    experience_years = forms.IntegerField(required=True)
+
+    class Meta:
+
+        model = Consultant
+        fields = ['first_name', 'last_name', 'experience_years']
+
+class PracticeAreaForm(forms.ModelForm):
+    """
+    Description:    The second consultant creation form. This populates
+                    the practice area field.
+    """
+    practice_areas = forms.ModelMultipleChoiceField(queryset=PracticeArea.objects.all(), required=True)
+
+    class Meta:
+        model = Consultant
+        fields = ['practice_areas']
+
+class SpecializationForm(forms.ModelForm):
+    """
+    Description:    The third consultant creation form. This populates
+                    the specialization field.
+    """
+    class Meta:
+        model = Consultant
+        fields = ['specializations']
+
+    def __init__(self, consultant_id, *args, **kwargs):
+        super(SpecializationForm, self).__init__(*args, **kwargs)
+        self.fields['specializations'].queryset = Specialization.objects.filter()
