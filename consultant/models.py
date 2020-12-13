b6b7ccbbdf9d68c4b22f6bb10f1c1ee1435553d7
faaiz <faaizajaz@gmail.com>
2020-12-13 16:35:16 +0500

    Added day rate and previous engagement fields

--- consultant/models.py
+++ consultant/models.py
@@ -18,6 +18,7 @@ class Consultant(models.Model):
     specializations = models.ManyToManyField(Specialization, blank=True)
     skills = models.ManyToManyField(Skill, blank=True)
     cv_file = models.FileField(blank=True, null=True)
+    day_rate = models.IntegerField(verbose_name="Daily rate in PKR")
 
     #####################
     # Computed fields
@@ -26,10 +27,11 @@ class Consultant(models.Model):
     date_created = models.DateField(verbose_name="Date consultant was added", auto_now=True)
     date_updated = models.DateField(verbose_name="Date of last update", auto_now=True)
     form_complete = models.BooleanField(default=False)
+    previous_engagement = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
 
     def get_absolute_url(self):
         from django.urls import reverse
-        return reverse
+        return reverse('consultant-view', args=[str(self.id)])
