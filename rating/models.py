e15fd5d27870d345c303d94cccae0b9477cf54c9
faaiz <faaizajaz@gmail.com>
2020-12-13 16:45:35 +0500

    Added days worked and contracted rate fields to Rating

--- rating/models.py
+++ rating/models.py
@@ -14,6 +14,21 @@ class Rating(models.Model):
     project_code = models.CharField(verbose_name="OPM project code", max_length=10)
     star_rating = models.IntegerField(verbose_name="Rating from 1-5", default=0)
     comment = models.TextField(verbose_name="Additional comments")
+    date_of_engagement = models.DateField(
+        verbose_name="The date of engagement",
+        null=True,
+        blank=True
+    )
+    days_worked = models.IntegerField(
+        verbose_name="Number of days contracted",
+        null=True,
+        blank=True
+    )
+    contracted_rate = models.IntegerField(
+        verbose_name="Rate at which consultant was contracted",
+        null=True,
+        blank=True
+    )
 
 
     def __str__(self):
