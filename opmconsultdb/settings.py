e9dd08ba2b4a6997a572f502ed82dcb61601219e
faaiz <faaizajaz@gmail.com>
2021-05-24 18:34:23 +0500

    Added postgres

--- opmconsultdb/settings.py
+++ opmconsultdb/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -42,7 +43,8 @@ INSTALLED_APPS = [
     'expertise.apps.ExpertiseConfig',
     'query.apps.QueryConfig',
     'rest_framework',
-    'crispy_forms'
+    'crispy_forms',
+    'gunicorn'
 ]
 
 MIDDLEWARE = [
@@ -80,11 +82,17 @@ WSGI_APPLICATION = 'opmconsultdb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+# FOR DEPLOYMENT
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+  'default': dj_database_url.config()
 }
 
 
