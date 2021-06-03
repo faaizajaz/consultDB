36f1b3602b4d7fad04d93069c1422e79be7ff0db
faaiz <faaizajaz@gmail.com>
2021-06-03 13:44:07 +0500

    Dev and added postgres app

--- opmconsultdb/settings.py
+++ opmconsultdb/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'query.apps.QueryConfig',
     'rest_framework',
     'crispy_forms',
-    'gunicorn'
+    'gunicorn',
+    'django.contrib.postgres'
 ]
 
 MIDDLEWARE = [
@@ -83,32 +84,32 @@ WSGI_APPLICATION = 'opmconsultdb.wsgi.application'
 
 ##########################################CHANGE FOR DEPLOY##########################################
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/storage'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = '/storage'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-# DEBUG = True
+# DEBUG = False
+DEBUG = True
 
-# DATABASES = {
-#     'default': {
-#        'ENGINE': 'django.db.backends.postgresql',  # noqa: E121
-#        'NAME': 'opmconsultdb_dev',
-#        'USER': 'faaiz',
-#        'PASSWORD': 'Outpo3t33',
-#        'HOST': '',
-#        'PORT': '',  # this seems to be default that the server wants
-#    }  # noqa: E122
-# }
+DATABASES = {
+    'default': {
+       'ENGINE': 'django.db.backends.postgresql',  # noqa: E121
+       'NAME': 'opmconsultdb_dev',
+       'USER': 'faaiz',
+       'PASSWORD': 'Outpo3t33',
+       'HOST': '',
+       'PORT': '',  # this seems to be default that the server wants
+   }  # noqa: E122
+}
 
 # FOR DEPLOYMENT
-DATABASES = {
-    'default': dj_database_url.config()
-}
+# DATABASES = {
+#     'default': dj_database_url.config()
+# }
 
 
 
